d861458ff7819dfb7bbee7b1c5bab07881b04756
woojook <woojook@andrew.cmu.edu>
2025-12-14 02:12:53 +0000

    alpha .2 graph

--- analysis.py
+++ analysis.py
@@ -12,9 +12,9 @@ cols_to_use = ['temp', 'RH', 'wind', 'rain', 'FFMC', 'DMC', 'DC', 'ISI', 'log_ar
 data = df[cols_to_use].copy()
 data_array = data.to_numpy()
 
-cg = pc(data_array, alpha=0.1)
+cg = pc(data_array, alpha=0.2)
 
 labels = [f"{col}" for col in data.columns]
 pyd = GraphUtils.to_pydot(cg.G, labels=labels)
-pyd.write_png('forest_fire_graph.png')
+pyd.write_png('forest_fire_graph2.png')
 
