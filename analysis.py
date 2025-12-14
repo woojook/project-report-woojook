e857351e00bbb74ba05e266233c0262ae9d172b4
woojook <woojook@andrew.cmu.edu>
2025-12-14 02:04:04 +0000

    log transformation for area

--- analysis.py
+++ analysis.py
@@ -4,8 +4,10 @@ import matplotlib.pyplot as plt
 from causallearn.search.ConstraintBased.PC import pc
 from causallearn.utils.GraphUtils import GraphUtils
 
-url = "forestfires.csv"
-df = pd.read_csv(url)
+df = pd.read_csv("forestfires.csv")
 
-print("Data loaded successfully.")
-print(df.head())
+df['log_area'] = np.log(df['area'] + 1)
+
+cols_to_use = ['temp', 'RH', 'wind', 'rain', 'FFMC', 'DMC', 'DC', 'ISI', 'log_area']
+data = df[cols_to_use].copy()
+data_array = data.to_numpy()
