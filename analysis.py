6ee771c14bdcaaccbc57a3078d4efd6c824fbb06
woojook <woojook@andrew.cmu.edu>
2025-12-14 02:05:14 +0000

    pc algo and graph

--- analysis.py
+++ analysis.py
@@ -11,3 +11,9 @@ df['log_area'] = np.log(df['area'] + 1)
 cols_to_use = ['temp', 'RH', 'wind', 'rain', 'FFMC', 'DMC', 'DC', 'ISI', 'log_area']
 data = df[cols_to_use].copy()
 data_array = data.to_numpy()
+
+cg = pc(data_array, alpha=0.05)
+
+labels = [f"{col}" for col in data.columns]
+pyd = GraphUtils.to_pydot(cg.G, labels=labels)
+pyd.write_png('forest_fire_graph.png')
