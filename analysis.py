0d2a48428ffb3e5f0edf507ef5441cb3180badc2
woojook <woojook@andrew.cmu.edu>
2025-12-14 01:58:22 +0000

    setup data and imports

--- analysis.py
+++ analysis.py
@@ -1,2 +1,11 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+from causallearn.search.ConstraintBased.PC import pc
+from causallearn.utils.GraphUtils import GraphUtils
+
+url = "forestfires.csv"
+df = pd.read_csv(url)
+
+print("Data loaded successfully.")
+print(df.head())
